6854b462f36867231b4785019182beb40996d95d
Sourav Das <esarve.srv@gmail.com>
2020-10-18 16:07:50 +0600

    Updated Preference

--- app/src/main/java/com/sourav/bettere/fragments/FragmentSettings.kt
+++ app/src/main/java/com/sourav/bettere/fragments/FragmentSettings.kt
@@ -4,12 +4,15 @@
 
 package com.sourav.bettere.fragments
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.text.InputType
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.sourav.bettere.R
 import com.sourav.bettere.utils.Constants
+
 
 class FragmentSettings : PreferenceFragmentCompat() {
 
@@ -23,6 +26,7 @@ class FragmentSettings : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.preference_ui, rootKey)
         val cdTimePref = findPreference<EditTextPreference>("prefkey_cdtime")
         val sampleRatePref = findPreference<EditTextPreference>("prefkey_samplerate")
+        val dummyPrefVersion = findPreference<Preference>("pref_version")
         cdTimePref!!.dialogMessage = Constants.PREF_DIALOG_TITLE_CDTIME
         cdTimePref.setOnBindEditTextListener { editText ->
             editText.inputType = InputType.TYPE_CLASS_NUMBER
@@ -32,6 +36,20 @@ class FragmentSettings : PreferenceFragmentCompat() {
         sampleRatePref.setOnBindEditTextListener { editText ->
             editText.inputType = InputType.TYPE_CLASS_NUMBER
         }
+
+        dummyPrefVersion!!.summary = getVersion()
+
+    }
+
+    private fun getVersion(): String {
+        try {
+            val pInfo =
+                requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
+            return pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        return ""
     }
 
 }
